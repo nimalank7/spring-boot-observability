87a26c53c0b0b706a5d12a6ebafede24e49dfcf7
nimalank7 <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2025-08-12 17:38:40 +0100

    Added log statement

--- src/main/java/com/application/micrometer/api/MessageController.java
+++ src/main/java/com/application/micrometer/api/MessageController.java
@@ -4,9 +4,14 @@ import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class MessageController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
+
     private Counter counter;
 
     public MessageController(MeterRegistry meterRegistry) {
@@ -15,6 +20,7 @@ public class MessageController {
 
     @GetMapping("/message")
     public String getMessage() {
+        logger.info("/message has been visited");
         counter.increment();
         return "Working...!!";
     }
